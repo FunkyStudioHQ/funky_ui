c6ef9da9f1df66811f83d6ed4d63e995f2fb8ac5
Lorenzo Pagano <lorenzo.pagano@gmail.com>
2017-10-20 14:50:37 +0200

    change folder order

--- core/js/index.js
+++
@@ -1,7 +0,0 @@
-import CloseNotification  from "./components/close-notification"
-import InputUpload        from "./components/input-upload"
-
-export {
-  CloseNotification,
-  InputUpload
-}
---
+++ index.js
@@ -0,0 +1,7 @@
+import CloseNotification  from "core/js/components/close-notification"
+import InputUpload        from "core/js/components/input-upload"
+
+export {
+  CloseNotification,
+  InputUpload
+}
