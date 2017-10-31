c3a7a7c16e5dbffcfc8b9f889436cd436a58a7ba
Lorenzo Pagano <lorenzo.pagano@gmail.com>
2017-10-31 11:26:03 +0100

    add tabs with js and bem syntax

--- js/components/tabs.js
+++ js/components/tabs.js
@@ -1,4 +1,4 @@
-let tabs = document.querySelectorAll(".tabs, .tabs--bar, .tabs--boxed")
+let tabs = document.querySelectorAll(".tabs")
 
 tabs.forEach = (tab) => {
   [].forEach.call(tabs, tab);
@@ -6,10 +6,20 @@ tabs.forEach = (tab) => {
 
 export default function(){
   tabs.forEach((tab) => {
-    let items = tab.querySelectorAll("ul li a")
-    items.forEach((item) => {
+    let items = tab.querySelectorAll(".tabs__list > .tabs__list__item")
+    let panels = tab.querySelectorAll(".tabs__content > .tabs__content__item")
+    items.forEach((item, i) => {
       item.addEventListener('click', (e) => {
-        console.log(1)
+        // remove active classes
+        [].forEach.call((items), function(el) {
+          el.classList.remove("tabs__list__item--active");
+        })
+        panels.forEach((item) => {
+          item.classList.remove("tabs__content__item--active")
+        })
+        // add active classes
+        e.target.parentElement.classList.add("tabs__list__item--active")
+        panels[i].classList.add("tabs__content__item--active")
       }, true)
     })
   })
